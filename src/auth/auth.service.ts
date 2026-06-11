93df4210db819d660223c4cc77878f2c4984de77
mcqeems <mcqeemsofficial@gmail.com>
2026-06-11 11:24:51 +0700

    add env example and auth controller

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
 
-  async signIn(username: string, password: string): Promise<any> {
+  async signIn(
+    username: string,
+    password: string,
+  ): Promise<{ access_token: string }> {
     const user = await this.usersService.findOne(username);
-
-    return user;
+    if (user?.password !== password) {
+      throw new UnauthorizedException();
+    }
+    const payload = { sub: user.id, username: user.username };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
   }
 }
