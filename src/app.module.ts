8a716a440aee4238dee6ac3328adf6394e917ef0
mcqeems <mcqeemsofficial@gmail.com>
2026-06-11 16:09:24 +0700

    add events and participants resource (crud)

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,15 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { EventsModule } from './events/events.module';
+import { ParticipantsModule } from './participants/participants.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     AuthModule, 
     UsersModule, 
-    PrismaModule
+    PrismaModule, EventsModule, ParticipantsModule
   ],
   controllers: [AppController],
   providers: [AppService],
