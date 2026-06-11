03b0d0b62d8a5dabdc4fb8250268817211f8df28
mcqeems <mcqeemsofficial@gmail.com>
2026-06-11 11:55:27 +0700

    fix the auth

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,18 @@
 import { Controller, Get, Render, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { UsersService } from './users/users.service';
 import { type Response } from 'express';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly usersService: UsersService,
+  ) {}
 
   @Get()
-  main(@Res() res: Response) {
-    return res.render(this.appService.getHello(), { message: 'Hello World' });
+  async main(@Res() res: Response) {
+    const data = await this.usersService.findOne('mustaqim');
+    return res.render(this.appService.getHello(), { message: data });
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -6,7 +7,12 @@ import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule, 
+    UsersModule, 
+    PrismaModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
