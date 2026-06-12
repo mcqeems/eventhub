c8750c8164068d2d25e656e8722587f54bf5e968
mcqeems <mcqeemsofficial@gmail.com>
2026-06-12 13:55:55 +0700

    add view engine, css and layout

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,43 @@
-import { Controller, Get, Render, Res } from '@nestjs/common';
-import { AppService } from './app.service';
-import { UsersService } from './users/users.service';
-import { type Response } from 'express';
+import { Controller, Get, Render } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private readonly usersService: UsersService,
-  ) {}
-
   @Get()
-  async main(@Res() res: Response) {
-    const data = await this.usersService.findOne('mustaqim');
-    return res.render(this.appService.getHello(), { message: data });
+  @Render('index')
+  home() {
+    return {
+      title: 'Home Page',
+      user: 'John Doe',
+    };
+  }
+
+  @Get('sign-in')
+  @Render('index')
+  signIn() {
+    return {
+      layout: 'layouts/auth',
+      title: 'Standalone Page',
+      user: 'Guest',
+    };
+  }
+
+  @Get('sign-up')
+  @Render('index')
+  signUp() {
+    return {
+      layout: 'layouts/auth',
+      title: 'Standalone Page',
+      user: 'Guest',
+    };
+  }
+
+  @Get('panel')
+  @Render('index')
+  panel() {
+    return {
+      layout: 'layouts/panel',
+      title: 'Standalone Page',
+      user: 'Guest',
+    };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,17 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'node:path';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
+import hbs from 'hbs';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
   app.useStaticAssets(join(__dirname, '..', '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', '..', 'views'));
   app.setViewEngine('hbs');
+  hbs.registerPartials(join(__dirname, '..', '..', 'views', 'layouts'));
+  app.setLocal('layout', 'layouts/main');
+
   app.use(cookieParser());
 
   await app.listen(process.env.PORT ?? 3000);
